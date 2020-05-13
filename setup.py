3786f456023ed4ede3dfe10b7ea5cd2d0d463e46
Enrica Loria <enricaloria@Enricas-MBP.homenet.telecomitalia.it>
2020-05-13 19:05:10 +0200

    setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'enrlor',                   # Type in your name
   author_email = 'enricaloria94@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/enrlor/sinfpy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/enrlor/sinfpy/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/enrlor/sinfpy/archive/v_01.tar.gz',   
   keywords = ['sna', 'graph', 'influence', 'game analytics'],   # Keywords that define your package best
   install_requires=[
         'numpy',
