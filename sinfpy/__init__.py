45e3e2814dfc269769a44db5c35d00d055a5c1e7
Enrica Loria <enricaloria@Enricas-MBP.homenet.telecomitalia.it>
2020-06-04 16:27:52 +0200

    update

--- sinfpy/__init__.py
+++ sinfpy/__init__.py
@@ -1,5 +1,3 @@
-from sinfpy.utils import similarity
 from sinfpy.semantic_influence import EdgeInfluence
 from sinfpy.semantic_influence import NodeInfluence
-from sinfpy.utils import number_of_peaks
-from sinfpy.utils import retention_transfer
+from sinfpy.characterization_metrics import retention_transfer
